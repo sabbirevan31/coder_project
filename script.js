30f67bf20cefa861fe06d2075359672acc3ada7b
sabbirevan31 <evan.cnits@gmail.com>
2023-12-20 14:35:48 +0600

    3rd commit

--- script.js
+++ script.js
@@ -15,6 +15,24 @@ $("#slider").owlCarousel({
       },
     },
   });
+
+$("#slider2").owlCarousel({
+    loop: true,
+    margin: 15,
+    nav: false,
+    autoplay: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 4,
+      },
+    },
+  });
   
 
   $(window).scroll(function(){
